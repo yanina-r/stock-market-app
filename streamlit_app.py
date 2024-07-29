c8b6ff0b1b334cdb06307f381b27aa751636a007
Yana Romaniv <romaniv1205@gmail.com>
2024-07-29 18:06:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,19 @@
 import streamlit as st
+import pandas as pd
+import yfinance as yf
 
-st.title("Market Volatility Analysis Project")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.set_page_config(
+    page_title="Stock Market")
+
+ticker_aapl = yf.Ticker("AAPL")
+data_aapl = ticker_aapl.history(period="5y")
+
+plt.figure(figsize=(10, 6))
+plt.plot(data_amzn.index, data_amzn['Close'], label='AMZN')
+plt.title('Price Amazon (AMZN)')
+plt.xlabel('Date')
+plt.ylabel('Closing price ($)')
+plt.legend()
+plt.show()
+
+ 
